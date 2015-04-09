b540f824bdf11479a55734a218d279202e2cc997
Garrett Thornburg <film42@gmail.com>
2015-04-09 14:38:33 -0600

    limit the intact during sync to 1000 days

--- app/workers/stock_sync_worker.rb
+++ app/workers/stock_sync_worker.rb
@@ -8,7 +8,8 @@ class StockSyncWorker
   private
 
   def batch_download_all_from_yahoo(stock_symbol)
-    raw_dataset = ::YahooFinance.historical_quotes(stock_symbol)
+    query = { :start_date => 1000.days.ago.midnight, :end_date => ::DateTime.now }
+    raw_dataset = ::YahooFinance.historical_quotes(stock_symbol, query)
     raw_dataset.each do |quote|
       stock_price = StockPrice.where(:label => stock_symbol,
                                      :quote_for => quote.trade_date.to_datetime)
